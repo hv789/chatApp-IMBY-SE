ff10fc29df08c13b7cd7a9938cfdc210d4cde0a4
Harsha Vardhan <hv2003789@gmail.com>
2023-09-26 21:49:04 +0530

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-1 text-6xl font-extrabold text-blue-500">EchoChat</h1>
-      <Image src={logo} alt="EchoChat" className="w-40 h-40" />
+    <div className="flex h-screen flex-col items-center justify-center space-y-10">
+      <Image src={logo} alt="EchoChat" className="h-40 w-40" />
+      <h1 className="mb-1 text-6xl font-mono font-extrabold text-blue-500">EchoChat</h1>
       <Button as={Link} href="/chat">
         Start chatting
       </Button>
